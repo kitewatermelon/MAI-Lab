88eec7312de43e73a39aae1af01f738a1b937014
YSPark <parkite576@gmail.com>
2024-01-02 17:26:45 +0900

    연습용 리스트 만들었는데 저기서 필요한거만 삭제하는 식으로 해야할 거 같음

--- NCP_CC_CCII/pratice.py
+++ NCP_CC_CCII/pratice.py
@@ -1,2 +1,36 @@
-from tensorflow.python.client import device_lib
-print(device_lib.list_local_devices())
+import pandas as pd
+import os
+
+file_path = []
+dir_path = "NCP_CC_CCII\\NCP"
+for (root, directories, files) in os.walk(dir_path):
+    for file in files:
+        file_path.append(os.path.join(root, file))
+
+
+
+# 파일 경로
+lesion_clices_path = 'NCP_CC_CCII\lesions_slices.csv'
+praticelist_path = 'praticelist.csv'
+
+# 문자열 변경 함수
+def transform_string(input_string):
+    return input_string.replace('NCP_CC_CCII\\NCP\\', 'NCP/').replace('\\', '/')
+
+# lesion_clices.csv 파일 읽기
+lesion_df = pd.read_csv(lesion_clices_path)
+
+# 변환된 문자열을 저장할 리스트
+transformed_strings = []
+
+# 문자열 변경 및 새 리스트에 추가
+for index, row in lesion_df.iterrows():
+    original_string = row['imgpath']
+    transformed_string = transform_string(original_string)
+    transformed_strings.append(transformed_string)
+
+# praticelist.csv 파일에 저장할 데이터프레임 생성
+praticelist_df = pd.DataFrame({'imgpath': transformed_strings})
+
+# 변환된 데이터프레임을 praticelist.csv 파일에 저장
+praticelist_df.to_csv(praticelist_path, index=False)
